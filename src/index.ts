2e4bfdf232e3d47f98f8e47e22f47530a57c870d
Gabriel A <gabrielaug4@gmail.com>
2022-12-11 14:59:36 -0300

    :sparkles: initial implementation of Maps Js API

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,11 @@ const company = new Company();
 
 console.log(user);
 console.log(company);
+
+new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
