d60ae1fd209ea055936cec181281538d1aa35773
Gabriel A <gabrielaug4@gmail.com>
2022-12-11 13:54:19 -0300

    :sparkles: initial implementation of User class

--- src/User.ts
+++ src/User.ts
@@ -1,3 +1,17 @@
-class User {
-  
+import { faker } from "@faker-js/faker";
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    long: number;
+  };
+
+  constructor(){
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      long: parseFloat(faker.address.longitude())
+    };
+  }
 }
